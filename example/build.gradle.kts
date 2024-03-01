359c82c5a0c66f74564b9d0a5f71ebd5d14df06e
Minsoo Cheong <icycle0409@snu.ac.kr>
2024-03-01 16:34:05 +0900

    add protobuf-java-util to example dependencies

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
 //    implementation("io.grpc:grpc-protobuf:${rootProject.ext["grpcJavaVersion"]}")
     // https://mvnrepository.com/artifact/com.google.protobuf/protobuf-java
     implementation("com.google.protobuf:protobuf-java:${rootProject.ext["protobufVersion"]}")
-
+    implementation("com.google.protobuf:protobuf-java-util:${rootProject.ext["protobufVersion"]}")
     implementation("io.grpc:grpc-stub:${rootProject.ext["grpcJavaVersion"]}")
     implementation("io.grpc:grpc-kotlin-stub:${rootProject.ext["grpcKotlinVersion"]}")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${rootProject.ext["coroutinesVersion"]}")
